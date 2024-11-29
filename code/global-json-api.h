66ba9624f839a6dfc67bdafb821c78b656bf2b67
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-11-29 18:14:29 +0100

    Update global-json-api.h

--- code/global-json-api.h
+++ code/global-json-api.h
@@ -1,1 +1,30 @@
+// global libs inclusion
 
+// C shenanigans
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+// MariaDB headers
+#include <mysql.h>
+#include <mysql/plugin.h>
+
+// micro httpd headers
+#include <microhttpd.h>
+
+// cJSON headers
+#include <cjson/cJSON.h>
+
+// global variables sharing
+// Defining the resources we want to check
+// TODO : ability to declare the list in a config file / system variable
+const char *resources[] = {
+    "/v1/",
+    "/v1/status/",
+    "/v1/tables/",
+    "/v1/procedures/"
+};
+const int num_resources = sizeof(resources) / sizeof(resources[0]);
+
+// global connexion : no creds, full privileges
+static MYSQL *global_mysql;
