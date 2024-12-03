f16cd18619c403bf1874a9a2e2842143e7ecdcfc
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-12-03 17:31:39 +0100

    Update handle_put_request.c

--- code/handle_put_request.c
+++ code/handle_put_request.c
@@ -1,39 +1,52 @@
 //include
 #include "global-json-api.h"
 
-static char* handle_put_request(const char *url, const char *upload_data, size_t *upload_data_size) {
+static char* handle_put_request(const char *url, const char *request_body, size_t *request_body_size) {
   // initializing the variables 
   char query[512]="";
 
   // initialize the JSON answer
   cJSON *json = cJSON_CreateObject();
   
-  if (sscanf(url, "/v1/procedures/%64[^/]/%64s", schema, procname) == 4) {  
-    
+  if (sscanf(url, "/v1/procedures/%64[^/]/%64s", schema, procname) == 2) {  
+        // TODO : check that ressource is exposed    
+        if(!is_exposed(url))
+        {
+           fprintf(stderr, "resource not exposed\n");
+           cJSON_AddStringToObject(json, "status", "RESOURCE not exspoed");
+           cJSON_AddStringToObject(json, "code","");
+          // clean exit procedure         
+          char *json_string = cJSON_PrintUnformatted(json);
+          cJSON_Delete(json);
+          return json_string; // Caller is responsible for freeing this memory
+        }
         // Here query database
-        snprintf(query, sizeof(query), "SELECT * FROM %s.%s WHERE %s = '%s'", schema, table, colname, colvalue);
+        snprintf(query, sizeof(query), "'use %s; CALL %s('", schema, procname);
+        // looping over request body to extract 
         cJSON_AddStringToObject(json, "source", "tables");
         cJSON_AddStringToObject(json, "schema", schema);
-        cJSON_AddStringToObject(json, "table", table);
-        cJSON_AddStringToObject(json, "column-name", colname);
-        cJSON_AddStringToObject(json, "column-value", colvalue);
+        cJSON_AddStringToObject(json, "procname", procname);
 
         MYSQL *connection = mysql_init(NULL);
         if (mysql_real_connect_local(connection) == NULL) { 
            fprintf(stderr, "mysql_init/mysql_real_connect_local failed\n");
            cJSON_AddStringToObject(json, "status", "CONNECTION failed");
-           char *json_string = cJSON_PrintUnformatted(json);
-           cJSON_Delete(json);
-           return json; // Caller is responsible for freeing this memory
+           cJSON_AddStringToObject(json, "code","");
+          // clean exit procedure         
+          char *json_string = cJSON_PrintUnformatted(json);
+          cJSON_Delete(json);
+          return json_string; // Caller is responsible for freeing this memory
         }
       
         if (mysql_real_query(connection, STRING_WITH_LEN(query))) {
            fprintf(stderr, "mysql_query() failed\n");
            mysql_close(connection);
            cJSON_AddStringToObject(json, "status", "QUERY failed");
-           char *json_string = cJSON_PrintUnformatted(json);
-           cJSON_Delete(json);
-           return json; // Caller is responsible for freeing this memory
+           cJSON_AddStringToObject(json, "code","");
+          // clean exit procedure         
+          char *json_string = cJSON_PrintUnformatted(json);
+          cJSON_Delete(json);
+          return json_string; // Caller is responsible for freeing this memory
         }
         
        MYSQL_RES *resultset = mysql_store_result(connection);
@@ -41,10 +54,12 @@ static char* handle_put_request(const char *url, const char *upload_data, size_t
           fprintf(stderr, "mysql_store_result() failed\n");
           mysql_close(conn);
           cJSON_AddStringToObject(json, "status", "RESULT failed");
+          cJSON_AddStringToObject(json, "code","");
+          // clean exit procedure         
           char *json_string = cJSON_PrintUnformatted(json);
           cJSON_Delete(json);
           mysql_free_result(resultset);
-          return json; // Caller is responsible for freeing this memory
+          return json_string; // Caller is responsible for freeing this memory
        }
 
         if (mysql_num_rows(resultset) > 0) {
@@ -68,18 +83,29 @@ static char* handle_put_request(const char *url, const char *upload_data, size_t
                 cJSON_AddItemToArray(rows_array, row_object);
            }
           cJSON_AddItemToObject(json, "data", rows_array);
+          cJSON_AddStringToObject(json, "code","200");
+          // clean exit procedure
+          char *json_string = cJSON_PrintUnformatted(json);
+          cJSON_Delete(json);
           mysql_free_result(resultset);
-          mysql_close(connection);
-          return json;
+          return json_string; // Caller is responsible for freeing this memory;
         } else {
           cJSON_AddStringToObject(json, "status", "NO DATA FOUND");
           cJSON_AddNumberToObject(json, "rows", 0);
+          cJSON_AddStringToObject(json, "code","200");
+          // clean exit procedure
+          char *json_string = cJSON_PrintUnformatted(json);
+          cJSON_Delete(json);
           mysql_free_result(resultset);
-          mysql_close(connection);
-          return json:
+          return json_string; // Caller is responsible for freeing this memory
         }
 
     }
-      // request format is KO
-      return NULL;
-}
+  // request format is KO
+  cJSON_AddNumberToObject(json, "error", "Invalid PUT request");
+  cJSON_AddStringToObject(json, "code","200");
+  // clean exit procedure
+  char *json_string = cJSON_PrintUnformatted(json);
+  cJSON_Delete(json);
+  return json_string; // Caller is responsible for freeing this memory    
+  }
