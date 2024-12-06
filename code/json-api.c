5a606290e529812f7b3102edad0daec482394a2c
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-12-06 11:45:07 +0100

    Update json-api.c

--- code/json-api.c
+++ code/json-api.c
@@ -192,7 +192,7 @@ static char* handle_delete_request(const char *url) {
     return NULL;
 }
 
-static int request_handler(void *cls, struct MHD_Connection *connection,
+static MHD_Result request_handler(void *cls, struct MHD_Connection *connection,
                            const char *url, const char *method,
                            const char *version, const char *upload_data,
                            size_t *upload_data_size, void **con_cls) {
