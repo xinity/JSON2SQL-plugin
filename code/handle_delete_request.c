d2692c0ebeeea1921b81d3c5c437d20960cd2c47
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-12-11 16:34:00 +0100

    Update handle_delete_request.c

--- code/handle_delete_request.c
+++ code/handle_delete_request.c
@@ -1,6 +1,3 @@
-//include
-#include "common.h"
-
 static char* handle_delete_request(const char *url) {
 // initialize the JSON answer
     cJSON *json = cJSON_CreateObject();  
@@ -14,15 +11,15 @@ static char* handle_delete_request(const char *url) {
     cJSON_Delete(json);
     return json_string; // Caller is responsible for freeing this memory
 #else
+    char schema[64];  
+    char table[64];
+    char colname[64];
+    char colvalue[64];
     
 // check request format, parameter extraction & statement exec
   if (sscanf(url, "/v1/tables/%64[^/]/%64[^/]/%64[^/]/%64s", schema, table, colname, colvalue) == 4) {  
     // initializing the variables 
     char query[512]="";
-    char schema[64];  
-    char table[64];
-    char colname[64];
-    char colvalue[64];
 // building query
     snprintf(query, sizeof(query), "'DELETE FROM %s.%s WHERE %s = %s'", schema, table, colname, colvalue);
 // establish internal local connexion
