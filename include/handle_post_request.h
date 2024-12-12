c2cc65d33dbb9ae2d2b3b633d13418712dc3efe7
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-12-12 07:22:49 +0100

    Update handle_post_request.h

--- include/handle_post_request.h
+++ include/handle_post_request.h
@@ -1,8 +1,6 @@
 #ifndef HANDLE_POST_REQUEST_H
 #define HANDLE_POST_REQUEST_H
 
-#include "common.h"
-
 char* handle_post_request(const char *url, const char *upload_data, size_t *upload_data_size);
 
 #endif // HANDLE_POST_REQUEST_H
