07ead433d1f878cfc12d26c60bbd494f3b9be727
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-12-12 10:30:00 +0100

    Update handle_put_request.h

--- include/handle_put_request.h
+++ include/handle_put_request.h
@@ -1,6 +1,6 @@
 #ifndef HANDLE_PUT_REQUEST_H
 #define HANDLE_PUT_REQUEST_H
 
-char* handle_put_request(const char *url, const char *request_body, size_t *request_body_size)
+char* handle_put_request(const char *url, const char *request_body, size_t *request_body_size);
 
 #endif // HANDLE_PUT_REQUEST_H
