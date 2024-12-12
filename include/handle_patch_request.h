c74f151cd6a5096d41b4b76b82ad42c7f4871862
Sylvain Arbaudie <arbaudie.it@gmail.com>
2024-12-12 07:22:39 +0100

    Update handle_patch_request.h

--- include/handle_patch_request.h
+++ include/handle_patch_request.h
@@ -1,8 +1,6 @@
 #ifndef HANDLE_PATCH_REQUEST_H
 #define HANDLE_PATCH_REQUEST_H
 
-#include "common.h"
-
 char* handle_patch_request(const char *url, const char *upload_data, size_t *upload_data_size);
 
 #endif // HANDLE_PATCH_REQUEST_H
